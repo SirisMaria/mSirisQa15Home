b2b6a9688313520f4b421ed21677d9286cee084a
Maria <siris230188@gmail.com>
2018-11-12 18:18:33 +0200

    Addressbook new project

--- addressbook-selenium-tests/src/test/java/com/telRan/addressbook/manager/ApplicationManager.java
+++ addressbook-selenium-tests/src/test/java/com/telRan/addressbook/manager/ApplicationManager.java
@@ -29,7 +29,7 @@ public class ApplicationManager {
             wd = new ChromeDriver();
         }else if(browser.equals(BrowserType.FIREFOX)){
             wd = new FirefoxDriver();
-        }else if(browser.equals(BrowserType.IE)){
+        }else if(browser.equals(BrowserType.CHROME )){
             wd = new EdgeDriver();
         }
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
